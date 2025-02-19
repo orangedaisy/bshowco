70498eb831bebdf18b264e9341762943ce8281b4
Bobby Showalter <bshow@Bobbys-Mac-mini.local>
2025-02-18 22:16:24 -0500

    Beautify blog post number

--- .eleventy.js
+++ .eleventy.js
@@ -18,6 +18,14 @@ module.exports = function (eleventyConfig) {
 	});
 	// Enable WebC
 	eleventyConfig.addPlugin(pluginWebc);
+	// Pad beginning of blog post number with leading zeros
+	eleventyConfig.addFilter('postNumber', (number) => {
+		// increment post number by 1 since collections start at 0
+		number++;
+		// convert the number to a localized string for proper separators then hit
+		// 'em with the padding
+		return number.toLocaleString().padStart(3, '0');
+	});
 
 	return {
 		dir: {
