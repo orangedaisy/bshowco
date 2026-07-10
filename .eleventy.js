bf20f65d0d7150fc2fde8e3580770e4973445bf8
Bobby Showalter <bobby.showalter@gmail.com>
2026-07-09 22:05:02 -0400

    Show 3 latest blog posts on homepage
    - Add `head()` filter to 11ty config
    - Show 3 posts instead of the title and excerpt for one post. Might change this
      again again again again after launching but I'd rather do this to get the
      site out the door more quickly

--- .eleventy.js
+++ .eleventy.js
@@ -30,6 +30,18 @@ module.exports = async function (eleventyConfig) {
 		return number.toLocaleString().padStart(3, '0');
 	});
 
+	// Get the first `n` elements of a collection.
+	eleventyConfig.addFilter("head", (array, n) => {
+		if(!Array.isArray(array) || array.length === 0) {
+			return [];
+		}
+		if( n < 0 ) {
+			return array.slice(n);
+		}
+
+		return array.slice(0, n);
+	});
+
 	// Import macros
 	eleventyConfig.addPreprocessor('macro-inject', '.njk,.md', (data, content) => {
 		return `
